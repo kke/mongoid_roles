dffcfc07e86c67a0f201a09258b9c232ba0ff528
Kimmo Lehto <kimmo.lehto@maventa.com>
2012-02-14 11:05:17 +0200

    find roles by object type

--- lib/mongoid/roles/role.rb
+++ lib/mongoid/roles/role.rb
@@ -14,6 +14,10 @@ module Mongoid
         object ?  where(:role => role, :auth_object_type => object.class.name, :auth_object_id => object.id) :
                   where(:role => role, :auth_object_type => nil, :auth_object_id => nil)
       }
+      scope :find_objects_by_type, lambda {|object_type| where(:auth_object_type => object_type)}
+      scope :find_objects_by_roles, lambda {|*roles| 
+        where(:role => {'$in' => roles.flatten})
+      }
       
       class << self
       
@@ -42,9 +46,21 @@ module Mongoid
         end
        
         # subject.roles_for(object). Returns an array of Role instances, corresponding to subject ’s roles on
-        # object. E.g. subject.roles_for(object).map(&:name).sort will give you role names in alphabetical order.
+        # object. E.g. subject.roles_for(object).map(&:role).sort will give you role names in alphabetical order.
+        # If called with a string, will return roles for all objects of that type. E.g. subject.roles_for('Group').
         def roles_for (object)
-          find_object(object)
+          if object.kind_of?(String) || object.kind_of(Symbol)
+            find_objects_by_type(object.to_s.singularize.camelcase)
+          else
+            find_object(object)
+          end
+        end
+
+        # subject.with_role(roles). Returns an array of Role instances, corresponding to subject ’s roles on
+        # object where the role name matches the given argument. Argument can also be a list of roles.. E.g. 
+        # subject.with_role(:admin, :member).map(&:role).sort will give you all 'admin' and 'member' roles.
+        def with_role (*roles)
+          find_objects_by_roles(roles)
         end
 
         # subject.has_no_roles_for!(object). Unassign any subject ’s roles for a given object.
@@ -56,8 +72,6 @@ module Mongoid
         def has_no_roles!
           criteria.destroy_all
         end
-
-        
         
         # def find_role(role, object = nil)
         #   object ? find_role_with_object(role,object) : find_role(role)
