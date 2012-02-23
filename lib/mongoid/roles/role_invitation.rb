83673de9ff9d508c4a6f199a711133dcca359ece
Kimmo Lehto <kimmo.lehto@maventa.com>
2012-02-23 14:50:40 +0200

    role invitations under work

--- lib/mongoid/roles/role_invitation.rb
+++ lib/mongoid/roles/role_invitation.rb
@@ -11,14 +11,6 @@ module Mongoid
       field :auth_subject_field,       :type => String
       field :auth_subject_value,       :type => String
       
-      class << self
-      
-      def auth_subject
-        @auth_subject ||= if auth_subject_type && auth_subject_value
-          auth_object_type.constantize.find(:conditions => {auth_subject_field => auth_subject_value})
-        end
-      end
-
     end
   end
 end
